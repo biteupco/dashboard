39fb11e0fd1ea2b08ba7d93d5358653c76517e1d
Kelvin Tay <kelvintaywl@gmail.com>
2015-10-03 19:00:07 +0900

    [master] add accounts page

--- dashboard/views/accounts.py
+++ dashboard/views/accounts.py
@@ -2,7 +2,9 @@
 from __future__ import division, print_function, absolute_import
 
 from flask.blueprints import Blueprint
-from flask import render_template
+from flask import render_template, session
+
+from dashboard.services.api import APIService
 
 
 blueprint = Blueprint('accounts', __name__)
@@ -10,4 +12,12 @@ blueprint = Blueprint('accounts', __name__)
 
 @blueprint.route('/')
 def index():
-    return render_template('account.html')
+    token = session.get('token')
+    user_id = session.get('user_id')
+
+    api_service = APIService(token)  # get user's SSO token from session
+    user_json = api_service.get_user(id=user_id)
+    params = {
+        'user': user_json
+    }
+    return render_template('account.html', **params)
